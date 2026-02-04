31517b353d1b105a5e8c6e27b4ee77bdf10cb983
Misalang, John Carl M. <johncarlmisalang@gmail.com>
2026-02-04 08:43:12 +0800

    Added section info for Misalang

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 print("Lab 1: Version Control")
-print("Author: [MISALANG, JOHN CARL M.]")
+print("Author: MISALANG, JOHN CARL .]")
 print("Student ID: TUPM-25-0478")
+print("Class Section: BSECE - 1A") #
